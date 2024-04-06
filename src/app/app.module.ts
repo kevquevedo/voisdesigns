95c1f7369e40bf2bcf8ce936280a057bc32daebb
unknown <quevedo.kevin@hotmail.com>
2024-04-06 10:49:18 -0300

    principal+navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { PresentacionComponent } from './componentes/presentacion/presentacion.component';
+import { ValoresComponent } from './componentes/valores/valores.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    PresentacionComponent,
+    ValoresComponent
   ],
   imports: [
     BrowserModule,
