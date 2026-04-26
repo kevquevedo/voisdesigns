96e5adf0ca5b54c368336d00856885f3c62d99d5
Kevin Quevedo <quevedo.kevin@hotmail.com>
2026-04-26 17:38:13 -0300

    retoques navbar, hero

--- src/app/components/nav/nav.ts
+++ src/app/components/nav/nav.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './nav.html',
   styleUrl: './nav.scss',
 })
-export class NavComponent {}
+export class NavComponent {
+  menuOpen = signal(false);
+  scrolled = signal(false);
+
+  toggleMenu() {
+    this.menuOpen.update(v => !v);
+  }
+
+  closeMenu() {
+    this.menuOpen.set(false);
+  }
+
+  @HostListener('window:scroll')
+  onScroll() {
+    this.scrolled.set(window.scrollY > 40);
+  }
+}
